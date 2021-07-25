acaf40ce6f560b39e8de50771b76f54776f8da4b
fe-olaf <frontend.olaf@gmail.com>
2021-07-25 12:29:04 +0900

    todo 예제 추가

---
+++ src/basic/condition.js
@@ -0,0 +1,18 @@
+// if, eles, switch
+
+/**
+ * if => 1~3개 정도면 if
+ * switch => 3개 이상이다 그러면 switch
+ */
+
+// 삼항 연산자
+
+const response = 10
+
+const message = response === 10 ? '성공입니다' : '실패입니다'
+
+// if (response === 10) {
+//   message = '성공입니다'
+// } else {
+//   message = '실패입니다'
+// }
